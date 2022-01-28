512ae9f9a335042a30cd14515c14627c5bdf7783
dyl-lee <dylanlee1701@gmail.com>
2022-01-27 23:10:02 -0500

    built bubble graph

--- static/js/chart.js
+++ static/js/chart.js
@@ -93,5 +93,27 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot('bar', barData, barLayout);
+    
+    // Bar and Bubble charts
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      mode: 'markers',
+      marker:{
+        size: sample_values,
+        color: otu_ids
+      },
+      text: otu_labels,
+    }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: 'Bacteria Cultures per Sample',
+      xaxis: {title: 'OTU ID'},
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot('bubble', bubbleData, bubbleLayout);
   });
 };
